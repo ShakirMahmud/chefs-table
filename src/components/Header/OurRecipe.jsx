7695e1daec7ae4a68f2f03ac6b8f72bddb9334f8
ShakirMahmud <shakirmahmud50@gmail.com>
2024-10-23 22:08:10 +0600

    navbar and banner section done

--- src/components/Header/OurRecipe.jsx
+++ src/components/Header/OurRecipe.jsx
@@ -1,8 +1,11 @@
 
 const OurRecipe = () => {
     return (
-        <div>
-            <h1>OurRecipe</h1>
+        <div className="text-center my-8 w-8/12 mx-auto space-y-4">
+            <h1 className="font-semibold text-5xl text-gray-800">Our Recipe</h1>
+            <p className="text-base text-gray-600">
+            Discover delicious, easy-to-make recipes that inspire your everyday meals. From quick breakfasts to hearty dinners, explore dishes that bring joy to your table.
+            </p>
         </div>
     );
 };
